5c8707389a2f9ca09cc984f37d0c6326f8ac26ce
abhisheknishad23 <iabhisheku4@gmail.com>
2026-06-25 23:12:21 +0530

    update home

--- Project/src/pages/HomePage.jsx
+++ Project/src/pages/HomePage.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import SearchBar from '../components/SearchBar'
 import Tabs from '../components/Tabs'
 import ResultGrid from '../components/ResultGrid'
+import { useSelector } from 'react-redux'
 
 const HomePage = () => {
+  const { query } = useSelector((store)=> store.search)
+
   return (
     <div>
+      <div className='text-center p-5 bg-blue-300 text-2xl font-semibold'>Media search</div>
         <SearchBar />
-        <Tabs />
-        <ResultGrid />
+        {query !=''? <div><Tabs />
+        <ResultGrid /></div>: ''}
     </div>
   )
 }
